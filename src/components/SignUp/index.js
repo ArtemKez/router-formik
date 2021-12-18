258f608698a57e2c02711edc417da3554035b54e
ArtemKez <artem.kez.work@gmail.com>
2021-12-18 12:57:07 +0200

    done sign up form

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -9,30 +9,34 @@ export default function SignUp() {
         text: "Sign In",
         to: "/"
     }
-    const SignInSchema = Yup.object().shape({
+    const SignUpSchema = Yup.object().shape({
         password: Yup.string()
             .min(6, 'Too Short Password!')
             .max(20, 'Too Long Password!')
             .required('Please Feel In Password'),
         passwordConfirmation: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Passwords must match')
             .min(6, 'Too Short Password!')
             .max(20, 'Too Long Password!')
-            .required('Please Feel In PasswordConfirmation'),
+            .required('Please Feel In Password confirmation'),
         emailAddress: Yup.string()
             .email('Invalid email')
             .required('Please Feel In Email'),
         firstName: Yup.string()
-            .min(6, 'Too Short Password!')
-            .max(20, 'Too Long Password!')
-            .required('Please Feel In FirstName'),
+            .min(6, 'Too Short First name!')
+            .max(20, 'Too Long First name!')
+            .required('Please Feel In First name'),
         lastName: Yup.string()
-            .min(6, 'Too Short Password!')
-            .max(20, 'Too Long Password!')
-            .required('Please Feel In LastName'),
+            .min(6, 'Too Short Last name!')
+            .max(20, 'Too Long Last name!')
+            .required('Please Feel In Last name'),
         displayName: Yup.string()
-            .min(6, 'Too Short Password!')
-            .max(20, 'Too Long Password!')
-            .required('Please Feel In DisplayName'),
+            .min(6, 'Too Short Display name!')
+            .max(20, 'Too Long Display name!')
+            .required('Please Feel In Display name'),
+        user_type: Yup
+            .string()
+            .required('Please Select User type')
     });
     return (
         <>
@@ -47,7 +51,8 @@ export default function SignUp() {
                         lastName: '',
                         displayName: '',
                         emailAddress: '',
-                        passwordConfirmation: ''
+                        passwordConfirmation: '',
+                        user_type: ''
                     }}
                     onSubmit={async (values, {setSubmitting}) => {
                         console.log(values)
@@ -55,7 +60,7 @@ export default function SignUp() {
                             setSubmitting(false);
                         }, 400);
                     }}
-                    validationSchema={SignInSchema}
+                    validationSchema={SignUpSchema}
                 >
                     {({
                           values,
@@ -63,105 +68,125 @@ export default function SignUp() {
                           touched,
                           handleSubmit,
                           isSubmitting,
-                      }) => (
-                        <Form className={"sign-in-form"} onSubmit={handleSubmit}>
-                            <div>
+                      }) => {
+                        return (
+                            <Form className={"sign-up-form"} onSubmit={handleSubmit}>
                                 <div>
-                                    <Field
-                                        placeholder={"firstName"}
-                                        className={`input ${errors.firstName && touched.firstName && 'error'}`}
-                                        type="text"
-                                        name="firstName"
-                                        value={values.firstName}
-                                    />
-                                    {errors.firstName && touched.firstName && errors.firstName}
-                                </div>
-                                <div>
-                                    <Field
-                                        placeholder={"lastName"}
-                                        className={`input ${errors.lastName && touched.lastName && 'error'}`}
-                                        type="text"
-                                        name="lastName"
-                                        value={values.lastName}
-                                    />
-                                    {errors.lastName && touched.lastName && errors.lastName}
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <Field
-                                        placeholder={"displayName"}
-                                        className={`input ${errors.displayName && touched.displayName && 'error'}`}
-                                        type="text"
-                                        name="displayName"
-                                        value={values.displayName}
-                                    />
-                                    {errors.displayName && touched.displayName && errors.displayName}
-                                </div>
-                                <div>
-                                    <Field
-                                        placeholder={"emailAddress"}
-                                        className={`input ${errors.email && touched.email && 'error'}`}
-                                        type="email"
-                                        name="emailAddress"
-                                        value={values.emailAddress}
-                                    />
-                                    {errors.email && touched.email && errors.email}
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <Field
-                                        placeholder={"password"}
-                                        className={`input ${errors.password && touched.password && 'error'}`}
-                                        type="password"
-                                        name="password"
-                                        value={values.password}
-                                    />
-                                    {errors.password && touched.password && errors.password}
-                                </div>
-                                <div>
-                                    <Field
-                                        placeholder={"passwordConfirmation"}
-                                        className={`input ${errors.password && touched.password && 'error'}`}
-                                        type="password"
-                                        name="passwordConfirmation"
-                                        value={values.passwordConfirmation}
-                                    />
-                                    {errors.password && touched.password && errors.password}
-                                </div>
-                            </div>
-                            <div className={"radio-inputs"}>
-                                <div className={"whiteBorder"}>
-                                    <Field
-                                        className={"inputRadio"}
-                                        type="radio"
-                                        name="radio1"
-                                    />
                                     <div>
-                                        <h2>Join As a Buyer</h2>
-                                        <p>I am looking for a Name, Logo or Tagline for my business, brand or
-                                            product.</p>
+                                        <Field
+                                            placeholder={"first name"}
+                                            className={`input ${errors.firstName && touched.firstName && 'error'}`}
+                                            type="text"
+                                            name="firstName"
+                                            value={values.firstName}
+                                        />
+                                        {errors.firstName && touched.firstName ?
+                                            <div className={"error_message"}>{errors.firstName}</div>
+                                            : ''}
+                                    </div>
+                                    <div>
+                                        <Field
+                                            placeholder={"last name"}
+                                            className={`input ${errors.lastName && touched.lastName && 'error'}`}
+                                            type="text"
+                                            name="lastName"
+                                            value={values.lastName}
+                                        />
+                                        {errors.lastName && touched.lastName ?
+                                            <div className={"error_message"}>{errors.lastName}</div>
+                                            : ''}
                                     </div>
                                 </div>
-                                <div className={"whiteBorder"}>
-                                    <Field
-                                        className={"inputRadio"}
-                                        type="radio"
-                                        name="radio2"
-                                    />
+                                <div>
                                     <div>
-                                        <h2>Join As a Creative or Marketplace Seller</h2>
-                                        <p>I plan to submit name ideas, Logo designs or sell names in Domain
-                                            Marketplace.</p>
+                                        <Field
+                                            placeholder={"display name"}
+                                            className={`input ${errors.displayName && touched.displayName && 'error'}`}
+                                            type="text"
+                                            name="displayName"
+                                            value={values.displayName}
+                                        />
+                                        {errors.displayName && touched.displayName ?
+                                            <div className={"error_message"}>{errors.displayName}</div>
+                                            : ''}
+                                    </div>
+                                    <div>
+                                        <Field
+                                            placeholder={"email address"}
+                                            className={`input ${errors.emailAddress && touched.emailAddress && 'error'}`}
+                                            type="email"
+                                            name="emailAddress"
+                                            value={values.emailAddress}
+                                        />
+                                        {errors.emailAddress && touched.emailAddress ?
+                                            <div className={"error_message"}>{errors.emailAddress}</div>
+                                            : ''}
                                     </div>
                                 </div>
-                            </div>
-                            <button className={"login"} type="submit" disabled={isSubmitting}>
-                                Create account
-                            </button>
-                        </Form>
-                    )}
+                                <div>
+                                    <div>
+                                        <Field
+                                            placeholder={"password"}
+                                            className={`input ${errors.password && touched.password && 'error'}`}
+                                            type="password"
+                                            name="password"
+                                            value={values.password}
+                                        />
+                                        {errors.password && touched.password ?
+                                            <div className={"error_message"}>{errors.password}</div>
+                                            : ''}
+                                    </div>
+                                    <div>
+                                        <Field
+                                            placeholder={"password confirmation"}
+                                            className={`input ${errors.passwordConfirmation && touched.passwordConfirmation && 'error'}`}
+                                            type="password"
+                                            name="passwordConfirmation"
+                                            value={values.passwordConfirmation}
+                                        />
+                                        {errors.passwordConfirmation && touched.passwordConfirmation ?
+                                            <div className={"error_message"}>{errors.passwordConfirmation}</div>
+                                            : ''}
+                                    </div>
+                                </div>
+                                <div className={`radio-inputs ${errors.user_type && touched.user_type && 'error'}`}>
+                                    <div className={"whiteBorder"}>
+                                        <Field
+                                            className={"inputRadio"}
+                                            type="radio"
+                                            name="user_type"
+                                            value="buyer"
+                                        />
+                                        <div>
+                                            <h2>Join As a Buyer</h2>
+                                            <p>I am looking for a Name, Logo or Tagline for my business, brand or
+                                                product.</p>
+                                        </div>
+                                    </div>
+                                    <div className={"whiteBorder"}>
+                                        <Field
+                                            className={"inputRadio"}
+                                            type="radio"
+                                            name="user_type"
+                                            value="marketplace"
+                                        />
+                                        <div>
+                                            <h2>Join As a Creative or Marketplace Seller</h2>
+                                            <p>I plan to submit name ideas, Logo designs or sell names in Domain
+                                                Marketplace.</p>
+                                        </div>
+                                    </div>
+                                    {errors.user_type && touched.user_type ?
+                                        <div className={"error_message"}>{errors.user_type}</div>
+                                        : ''}
+                                </div>
+                                <button className={"login"} type="submit" disabled={isSubmitting}>
+                                    Create account
+                                </button>
+                            </Form>
+                        )
+                    }
+                    }
                 </Formik>
             </div>
         </>
